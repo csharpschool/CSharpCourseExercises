995e56ef7ec8bf5807ad547006ae69c885ed6e7d
Jonas <info@csharpschool.com>
2019-09-28 22:31:11 +0200

    Renamed folders and namespaces

--- CSharpExercises/Classes/Chapter 03/Student.cs
+++ CSharpExercises/Classes/Chapter 03/Student.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CSharpExercises.Classes.Chapter_3
+namespace CSharpExercises.Classes.Chapter_03
 {
     public class Student
     {
