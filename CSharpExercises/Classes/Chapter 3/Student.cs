9ad6597b424276d825b4690914dc3f6aa03a30d5
Jonas <info@csharpschool.com>
2019-09-28 11:19:39 +0200

    Exercise 2 and 3

---
+++ CSharpExercises/Classes/Chapter 3/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpExercises.Classes.Chapter_3
+{
+    public class Student
+    {
+        private readonly string firstName = string.Empty;
+        private readonly string lastName = string.Empty;
+        public string fullName = string.Empty;
+        public readonly int age = default;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            fullName = $"{firstName} {lastName}";
+        }
+    }
+}
