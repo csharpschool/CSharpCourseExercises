44079d2bf9d2f7eadd7cc0f2be85c00aeb9091fa
Jonas <info@csharpschool.com>
2019-09-29 13:29:44 +0200

    Exercise 04-01 Calculator

---
+++ CSharpExercises/Classes/Chapter 04/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpExercises.Classes.Chapter_04
+{
+    public class Calculator
+    {
+        private string Output(double value1, double value2, double result, char @operator) 
+        {
+            return $"{value1} {@operator} {value2} = {result}";
+        }
+
+        public double Add(double value1, double value2)
+        {
+            var result = value1 + value2;
+            return result;
+        }
+        public double Add(double value1, double value2, out string output)
+        {
+            var result = Add(value1, value2);
+            output = Output(value1, value2, result, '+');
+            return result;
+        }
+
+        public double Subtract(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+        public double Subtract(double value1, double value2, out string output)
+        {
+            var result = Subtract(value1, value2);
+            output = Output(value1, value2, result, '-');
+            return result;
+        }
+
+        public double Divide(double value1, double value2)
+        {
+            // Note that division by 0 will result in 
+            // an unhandled exception (error)
+            return value1 / value2;
+        }
+        public double Divide(double value1, double value2, out string output)
+        {
+            var result = Divide(value1, value2);
+            output = Output(value1, value2, result, '/');
+            return result;
+        }
+
+        public double Multiply(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+        public double Multiply(double value1, double value2, out string output)
+        {
+            var result = Multiply(value1, value2);
+            output = Output(value1, value2, result, 'x');
+            return result;
+        }
+    }
+}
