2719a2f4051ab5298dd2728e581cfc5363a3d594
Jonas <info@csharpschool.com>
2019-10-02 12:57:43 +0200

    exercise-06-01: To-do

---
+++ CSharpExercises/Classes/Chapter 06/Todo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpExercises.Classes.Chapter_06
+{
+    public class Todo
+    {
+        private string description = string.Empty;
+        public string completed = string.Empty;
+        public List<KeyValuePair<string, object>> attributes = new List<KeyValuePair<string, object>>();
+
+        public Todo(string description)
+        {
+            this.description = description;
+        }
+
+        // Expression bodied methods
+        public string GetDescription() => description;
+        public List<KeyValuePair<string, object>> GetAttributes() => attributes;
+        public void CheckAttributes()
+        {
+            if (completed.Equals(string.Empty))
+                attributes.Clear();
+            else
+                attributes.Add(new KeyValuePair<string, object>("checked", "checked"));
+        }
+    }
+}
---
+++ CSharpExercises/Classes/Chapter 06/Todos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpExercises.Classes.Chapter_06
+{
+    public class Todos
+    {
+        List<Todo> todos = new List<Todo>();
+
+        public Todos()
+        {
+            Add("To-do 1");
+            Add("To-do 2");
+        }
+
+        public List<Todo> Get() => todos;
+
+        public void Add(string description)
+        {
+            todos.Add(new Todo(description));
+        }
+
+        public void Remove(Todo todo)
+        {
+            todos.Remove(todo);
+        }
+
+        //public void RemoveAt(int index)
+        //{
+        //    todos.RemoveAt(index);
+        //}
+
+        //public void SetCompleted(Todo todo)
+        //{
+        //    todo.SetCompleted();
+        //}
+
+        //public void SetNotCompleted(Todo todo)
+        //{
+        //    todo.SetNotCompleted();
+        //}
+
+    }
+}
